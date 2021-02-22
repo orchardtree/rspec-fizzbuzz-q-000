f819b5fc43ae883d0131735ade709548ff387b6e
Jesse Wenger <wengerj@gmail.com>
2021-02-22 02:54:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,9 @@
 def fizzbuzz (number)
   if number % 3 == 0
     "Fizz"
-    else if number % 5 == 0
+    elsif number % 5 == 0
     "Buzz"
-    end
-    else if number % 3 == 0 && number % 5 == 0 
+    elsif number % 3 == 0 && number % 5 == 0 
     "FizzBuzz"
-    end
   end
 end
